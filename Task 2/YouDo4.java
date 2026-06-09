03f99fa0cdabb0eac2f42abeb35c095966b0a30b
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-09 10:28:46 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,26 @@
 
+class Sensor{
+    String name;
+    int batteryLevel;
+
+    public Sensor(String name,int initialBattery){
+        this.name = name;
+        this.batteryLevel = initialBattery;
+    }
+
+    public void charge(){
+        this.batteryLevel = 100;
+        System.out.println(name + " sensor fully charged");
+    }
+}
+
+
+public class main4 {
+    public static void main(String[] args) {
+        Sensor proximitySensor = new Sensor("proximity", 45);
+
+        proximitySensor.charge();
+
+        System.out.println("Verified battery level: " + proximitySensor.batteryLevel + "%");
+    }
+}
