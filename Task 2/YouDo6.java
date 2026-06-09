92f8cefa10b6557296a9243d9522809b846fdff1
muaj13 <156597416+muaj13@users.noreply.github.com>
2026-06-09 11:24:46 +0600

    Add Applet and GameApplet classes with launch method

--- Task 2/YouDo6.java
+++ Task 2/YouDo6.java
@@ -1,1 +1,20 @@
+Class Applet {
+    void launch(){
+        System.out.println("Applet launched");
+        
+    }}
+    
+    class GameApplet extends Applet {
+        void play(){
+            System.out.println("Playing game);
+        }
+    }
+    
+    public class Main {
+    public static void main(String[] args) {
+        GameApplet myGame = new GameApplet();
+        myGame.launch();
+        myGame.play();
+    }
+}
 
