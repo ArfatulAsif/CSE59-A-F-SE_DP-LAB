3b3e95423ac86e6f0936412c55b6d85f8d2e9039
Mehedi Hasan <137675578+thatmehedi@users.noreply.github.com>
2026-06-23 11:26:02 +0600

    Add TeamMember, Employee, and Contractor classes

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,44 @@
+class TeamMember {
+    protected String name;
 
+    public TeamMember(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Employee extends TeamMember {
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    public void calculateBonus() {
+        System.out.println(name + " receives standard employee bonus.");
+    }
+}
+
+class Contractor extends TeamMember {
+
+    public Contractor(String name) {
+        super(name);
+    }
+
+    public void work() {
+        System.out.println(name + " is working as a contractor.");
+    }
+}
+
+public class main2 {
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("Alice");
+        emp.calculateBonus();
+
+        Contractor con = new Contractor("Bob");
+        con.work();
+    }
+}
