28d98e1c3433f274ca59e7b7dde2ff0dee8b03ec
Sadman Sakib Khan <sakib.sadman2015@gmail.com>
2026-06-23 09:51:27 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,48 @@
+import  java.util.Scanner;
 
+interface Shape {
+    double calculateArea();
+}
+
+class Rectangle implements Shape {
+    double length,width;
+    
+    public Rectangle(double length,double width) {
+        this.length = length;
+        this.width = width;
+    }
+    
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Circle implements Shape {
+    double radius;
+    
+    public Cirlce(double radius) {
+        this.radius = radius;
+    }
+    
+    
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius *radius;
+    }
+}
+
+class Truangle implements Shape {
+    double base,height;
+    
+    public Triangle(double base,double height) {
+        this.base = base;
+        this.height = height;
+        
+    }
+    
+    @Override 
+    public calculateArea() {
+        reuturn 0.5* base * height;
+    }
+}
