671defed73f403a2c7455e753a2deddc73d30d01
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 10:29:03 +0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,45 @@
 
+class Patient{
+    private String patientName;
+    private String nationalID;
+
+    public Patient(String patientName, String nationalID){
+        this.patientName = patientName;
+        this.nationalID = nationalID;
+    }
+
+    public String getPatientName(){
+        return patientName;
+    }
+
+    public String getNationalId(){
+        return nationalID;
+    }
+}
+
+
+class IdValidator{
+    public boolean validateId(String nationalID) {
+        return nationalID.length() == 10 || nationalID.length() == 17;
+    }
+}
+
+class SmsService  {
+    public void sendSms(Patient patient) {
+        System.out.println("Sending SMS to " + patient.getPatientName() + ": Registration Successful.");
+
+    }
+
+}
+public class Main3 {
+    public static void main(String[] args){
+        Patient patient = new Patient("Rahim", "123445");
+        IdValidator validator = new IdValidator();
+        SmsService sms = new SmsService();
+        if(validator.validateId(patient.getNationalId())){
+            sms.sendSms(patient);
+        } else {
+            System.out.println("Invalid National ID.");
+        }
+    }
+}
