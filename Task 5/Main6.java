e6c9c7e3d1ecb4a02d774b8b5c2995bb49abb001
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 11:06:51 +0600

    Implement interfaces for HTTP, DB, and Docker services

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,43 @@
 
+interface HttpService {
+    void handleHttpRequest();
+}
+
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+class webController implements HttpService {
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+class DataSerrvice implements DatabaseManager {
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL query.");
+    }
+}
+
+class DockerService implements ContainerOps {
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+public class Main6 {
+    public static void main(String[] args) {
+        HttpService controller = new WebController();
+        controller.handleHttpRequest();
+
+        DatabaseManager db = new DatabaseService();
+        db.executeSqlStatement();
+
+        ContainerOps docker = new DockerService();
+        docker.restartDockerContainer();
+
+    }
+}
