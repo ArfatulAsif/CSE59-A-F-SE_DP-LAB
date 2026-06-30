100243cc3cd60bf74c2db40148800fd8a580c5c6
mahmudafariha <140585966+mahmudafariha@users.noreply.github.com>
2026-06-30 11:03:58 +0600

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,47 @@
+interface INetworkProtocol {
+    void broadcastMessage(String msg);
+}
 
+class BluetoothMeshNetwork implements INetworkProtocol {
+
+
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via Bluetooth Mesh: " + msg);
+    }
+}
+
+class WifiDirectNetwork implements INetworkProtocol {
+
+
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via WiFi Direct: " + msg);
+    }
+}
+
+class DisasterAlertSystem {
+
+    private INetworkProtocol network;
+
+  
+    public DisasterAlertSystem(INetworkProtocol network) {
+        this.network = network;
+    }
+
+    public void triggerAlert(String alertData) {
+        network.broadcastMessage(alertData);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        INetworkProtocol bluetooth = new BluetoothMeshNetwork();
+        DisasterAlertSystem system1 = new DisasterAlertSystem(bluetooth);
+        system1.triggerAlert("Earthquake detected!");
+
+        INetworkProtocol wifi = new WifiDirectNetwork();
+        DisasterAlertSystem system2 = new DisasterAlertSystem(wifi);
+        system2.triggerAlert("Tsunami warning!");
+    }
+}
