5127b521d3e5cace71116fe9e0cc951ac99494a4
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-30 10:24:00 +0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,49 @@
 
+import javax.print.attribute.standard.PageRanges;
+
+class Patient {
+    private String patientName;
+    private String nationalId;
+
+    public Patient(String patientName, String nationalId) {
+        this.patientName = patientName;
+        this.nationalId = nationalId;
+    }
+
+    public String getPatientName(){
+        return patientName;
+    }
+
+    public String getNationalId(){
+        return nationalId;
+    }
+}
+
+class Idvalidator{
+    public boolean validateId(String nationalId){
+        return nationalId.length() == 10 || nationalId.length() == 17;
+    }
+}
+
+class SmsService{
+    public void sendSmsConfirmation(Patient patient){
+        Idvalidator validator = new Idvalidator();
+
+        if(validator.validateId(patient.getNationalId())){
+            System.out.println("Sending SMS to " + patient.getPatientName() + ": Registration successful.");
+        }
+
+        else{
+            System.out.println("Invalid National ID.");
+        }
+
+    }
+}
+
+public class Main3{
+    public static void main(String[] args) {
+        Patient patient = new Patient("John Doe", "1234567890");
+        SmsService smsService = new SmsService();
+        smsService.sendSmsConfirmation(patient);
+    }
+}
