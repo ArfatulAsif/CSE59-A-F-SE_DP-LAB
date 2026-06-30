10243ab71b8b84c6fd2b1829d6289f68bd53978b
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 10:07:20 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,41 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+class EmailSender implements MessageSender{
+    public void sendMessage(String message) {
+        System.out.println("Sending Email : "+message );
+    }
+}
+class SMSSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: "+message);
+    }
+}
+
+class NotificationService{
+    private MessageSender messageSender;
+
+    public NotificationService(MessageSender messageSender){
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg){
+        messageSender.sendMessage(msg);
+    }
+}
+
+public class Main2{
+    public static void main(String[] args){
+        MessageSender email = new EmailSender();
+
+        NotificationService service1 = new NotificationService(email);
+        service1.alertUser("Your order hasa been shipped");
+
+        System.out.println();
+
+        MessageSender sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alertUser("your OTP is 1234");
+    }
+}
