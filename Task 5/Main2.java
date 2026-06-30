49fca36846b1d95da462246187adf8337d477e89
muaj13 <156597416+muaj13@users.noreply.github.com>
2026-06-30 11:35:13 +0600

    Implement MessageSender interface with EmailSender youdo2

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,34 @@
 
+interface MessageSender{
+    void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender{
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("sending email:  "+message);
+    }
+}
+
+class NotificationServer {
+    private MessageSender messageSender;
+
+    public NotificationServer(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String message) {
+        messageSender.sendMessage(message);
+    }
+
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        MessageSender emailSender = new EmailSender();
+        NotificationServer notificationServer = new NotificationServer(emailSender);
+
+        notificationServer.alertUser("your order has been shipped");
+
+    }
+}
