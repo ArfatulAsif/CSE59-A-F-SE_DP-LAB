38d79ef4ed8f342a1e5cebde823199630ac385f9
muaj13 <156597416+muaj13@users.noreply.github.com>
2026-07-21 10:23:37 +0600

    Add HttpRequest class with Builder pattern
    Implemented HttpRequest class with Builder pattern for constructing HTTP requests.

--- Task 8/Main1.java
+++ Task 8/Main1.java
@@ -1,1 +1,48 @@
+    class HttpRequest {
+    private final String url;
+    private final String method;
+    private final String headers;
+    private final String body;
+
+    private HttpRequest(Builder builder) {
+        this.url = builder.url;
+        this.method = builder.method;
+        this.headers = builder.headers;
+        this.body = builder.body;
+    }
+
+    public void send () {
+        System.out.println("String" + method + "req to" + url);
+    }
+
+    public static class Builder {
+        private String url ;
+        private String method = "GET";
+        private String headers = "";
+        private String body = "";
+        
+        public Builder(String url) { this.url=url;}
+        public Builder setMethod(String method) {this.method = method; return this;}
+        public Builder addHeader(String header) { this.headers += header + ";"; return this; }
+        public Builder setBody(String body) { this.body = body; return this; }
+        
+        public HttpRequest build ()  {
+            return new HttpRequest(this);
+        }
+
+
+    }
+}
+
+public class p1ex1 {
+    public static void main(String[] args) {
+        HttpRequest req = new HttpRequest.Builder("https://api.example.com/data")
+        .setMethod("post")
+        .addHeader("Auth: Bearer token123")
+        .setBody("{'user_id': '404'}")
+        .build();
+
+    req.send();
+    }
+}
 
