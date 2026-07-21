1cec25e84af51a8266999d6439e2bd341e938209
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-07-21 11:28:11 +0600

    Update Main5.java

--- Task 8/Main5.java
+++ Task 8/Main5.java
@@ -1,1 +1,29 @@
 
+class ModelWeightsCache {
+    private static volatile ModelWeightsCache instance;
+
+    private ModelWeightsCache() {
+        System.out.println("Loading 2GB model weights into memory... (Cached successfully)");
+    }
+
+public static ModelWeightsCache getInstances() {
+    if(instance == null) {
+        synchronized (ModelWeightsCache.class){
+            if(instance == null) {
+                        instance = new ModelWeightsCache();
+                    }
+                }
+            }
+            return instances;
+        }
+        public void predict(String dataSample) {
+            System.out.println("Running inference on: " + dataSample);
+        }
+    }
+ 
+public class Main5 {
+    public static void main(String[] args){
+        ModelWeightsCache cache1 = ModelWeightsCache.getInstances();
+        cache1.predict("image_02.png");
+    }
+}
